b99d5e02565b05b19da790a2374d88f8d5846b9b
Vincent Zhang <ivincent.zhang@gmail.com>
2020-07-11 16:37:16 +0800

    Improvement: better LoadingP1D1FetchUrlQueue.

--- demo/src/main/java/ai/platon/demo/DemoApplication.java
+++ demo/src/main/java/ai/platon/demo/DemoApplication.java
@@ -5,23 +5,30 @@ import ai.platon.pulsar.PulsarContextKt;
 import ai.platon.pulsar.PulsarSession;
 import ai.platon.pulsar.dom.FeaturedDocument;
 import ai.platon.pulsar.persist.WebPage;
+import com.google.common.collect.Lists;
 import kotlin.Unit;
 import org.jsoup.nodes.Element;
+
+import java.lang.reflect.Array;
+import java.util.List;
+import java.util.stream.IntStream;
 
 public class DemoApplication {
     private String url = "https://list.jd.com/list.html?cat=652,12345,12349";
 
     public void run() throws Exception {
-        PulsarSession session = PulsarContext.createJvmSession();
-        WebPage page = session.load(url, "-expires 1d");
-        FeaturedDocument document = session.parse(page, false);
-        Element element = document.selectFirstOrNull(".sku-name");
+        var session = PulsarContext.createJvmSession();
+        var page = session.load(url, "-expires 1d");
+        var document = session.parse(page, false);
+        var element = document.selectFirstOrNull(".sku-name");
         if (element != null) {
-            String text = element.text();
+            var text = element.text();
             System.out.println(text);
         } else {
             System.out.println(document.getTitle());
         }
+
+        var r = IntStream.of(1, 2, 3, 4, 5).filter(i -> i > 1).mapToObj(i -> List.of(i, i * 2));
     }
 
     public static void main(String[] args) {
